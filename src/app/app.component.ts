e8f5bead34b592bfc1687ea886fdf3d37a3d02cf
pauline <pauline.feytel@gmail.com>
2018-05-24 14:46:29 +0200

    Add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my new app';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyATt5bvHWUUYfbPBeYRrhrIc0T0n8xN94I",
+      authDomain: "vocabulary-cd6bb.firebaseapp.com",
+      databaseURL: "https://vocabulary-cd6bb.firebaseio.com",
+      projectId: "vocabulary-cd6bb",
+      storageBucket: "",
+      messagingSenderId: "1017780856383"
+    };
+    firebase.initializeApp(config);
+  }
 }
