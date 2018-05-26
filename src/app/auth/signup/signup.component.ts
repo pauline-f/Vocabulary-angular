f2da37a2b61c33535b485abea6724095f7956466
pauline <pauline.feytel@gmail.com>
2018-05-26 21:34:04 +0200

    Add signIn and signUp forms

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { error } from 'util';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  signUpForm: FormGroup;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.signUpForm = this.formBuilder.group( {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit() {
+    const email = this.signUpForm.get('email').value;
+    const password = this.signUpForm.get('password').value;
+    this.authService.createNewUser(email, password).then (
+      () => {
+        this.router.navigate(['/words']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
   }
 
 }
