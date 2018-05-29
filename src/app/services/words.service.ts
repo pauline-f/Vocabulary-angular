4d031b0b33424614c72921dd54fa8ccde2d9d6de
pauline <pauline.feytel@gmail.com>
2018-05-29 14:22:42 +0200

    Add methods in word.service.ts

--- src/app/services/words.service.ts
+++ src/app/services/words.service.ts
@@ -1,8 +1,63 @@
 import { Injectable } from '@angular/core';
+import { Word } from '../models/Word.model';
+import { Subject } from 'rxjs/Subject';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class WordsService {
 
+  words: Word[] = [];
+  wordsSubject = new Subject<Word[]>();
+
   constructor() { }
+
+  emitWords() {
+    this.wordsSubject.next(this.words);
+  }
+
+  saveWords() {
+    firebase.database().ref('/words').set(this.words);
+  }
+
+  getWords() {
+    firebase.database().ref('/words')
+      .on('value', (data) => {
+        this.words = data.val() ? data.val() : [];
+        this.emitWords();
+      });
+  }
+
+  getSingleWord(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/words/+id').once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewWord(newWord: Word) {
+    this.words.push(newWord);
+    this.saveWords();
+    this.emitWords();
+  }
+
+  removeWords(word: Word) {
+    const wordIndexToRemove = this.words.findIndex(
+      (wordEl) => {
+        if (wordEl === word) {
+          return true;
+        }
+      }
+    );
+    this.words.splice(wordIndexToRemove, 1);
+    this.saveWords();
+    this.emitWords();
+  }
 
 }
