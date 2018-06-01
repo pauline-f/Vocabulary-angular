6437034ab68ca2d25db6fa313ea88730dd12b137
pauline <pauline.feytel@gmail.com>
2018-06-01 13:48:46 +0200

    Add the choice 'Add a new list' in word-form

--- src/app/word-list/word-form/word-form.component.ts
+++ src/app/word-list/word-form/word-form.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { WordsService } from '../../services/words.service';
 import { Router } from '@angular/router';
 import { Word } from '../../models/Word.model';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-word-form',
@@ -12,13 +13,35 @@ import { Word } from '../../models/Word.model';
 export class WordFormComponent implements OnInit {
 
   wordForm: FormGroup;
+  lists: String[];
+  listsSubscription: Subscription;
+  newList: boolean;
 
   constructor(private formBuilder: FormBuilder,
               private wordsService: WordsService,
               private router: Router) { }
 
   ngOnInit() {
+    this.listsSubscription = this.wordsService.listsSubject.subscribe(
+      (lists: String[]) => {
+        this.lists = lists;
+      }
+    );
+    this.wordsService.getWords();
+    this.newList = false;
     this.initForm();
+  }
+
+  addNewList() {
+    if (this.wordForm.get('list').value === "Add a new list") {
+      this.wordForm.get('list').setValue("");
+      this.newList = true;
+    } else {
+      this.newList = false;
+    }
+
+    
+    //this.initForm();
   }
 
   initForm() {
