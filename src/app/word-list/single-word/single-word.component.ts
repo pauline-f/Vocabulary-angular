3a46f66b65b5a79294b5f38f3d39329cfb811d02
pauline <pauline.feytel@gmail.com>
2018-06-24 20:40:00 +0200

    Add images (flags) in view single word

--- src/app/word-list/single-word/single-word.component.ts
+++ src/app/word-list/single-word/single-word.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Word } from '../../models/Word.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WordsService } from '../../services/words.service';
+import { LanguagesService } from '../../services/languages.service';
+import { Languages } from '../../models/Languages.model';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-single-word',
@@ -11,12 +14,28 @@ import { WordsService } from '../../services/words.service';
 export class SingleWordComponent implements OnInit {
 
   word: Word;
+  languages: Languages[];
+  baseLanguage: String;
+  languageToLearn: String;
+  languagesSubscription: Subscription;
 
   constructor(private route: ActivatedRoute,
               private wordsService: WordsService,
-              private router: Router) { }
+              private router: Router,
+              private languagesService: LanguagesService) { }
 
   ngOnInit() {
+    
+
+    this.languagesSubscription = this.languagesService.languagesSubject.subscribe(
+      (languages: Languages[]) => {
+        this.languages = languages;
+        this.baseLanguage = this.languages[0].baseLanguage;
+        this.languageToLearn = this.languages[0].languageToLearn;
+      }
+    );
+    this.languagesService.getLanguages();  
+
     this.word = new Word('', '', '');
     const id = this.route.snapshot.params['id'];
     this.wordsService.getSingleWord(+id).then(
